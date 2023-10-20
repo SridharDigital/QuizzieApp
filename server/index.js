2bb9e2afbeccc1ac617fcc8144093947a39ab67d
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2023-10-20 15:16:06 +0530

    Created API's for login and signup, sended jwtToken as response

--- server/index.js
+++ server/index.js
@@ -46,6 +46,89 @@ const Quiz = mongoose.model("Quiz", {
   answeredCorrectly: Number,
 })
 
+app.post("/api/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+    const isEmailTaken = await Admin.findOne({ email: email.toLowerCase() })
+    // console.log({ name, email, password })
+    // console.log(isEmailTaken)
+    if (isEmailTaken) {
+      throw "Email already in use"
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    // console.log("Hashed Password:", hashedPassword)
+
+    const newAdmin = {
+      name,
+      email: email.toLowerCase(),
+      password: hashedPassword,
+    }
+
+    const createdAdmin = await Admin.create(newAdmin)
+    console.log("Admin Details1:", createdAdmin)
+
+    const jwtToken = jwt.sign({ newAdmin }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "15d",
+    })
+
+    console.log("Admin Details2:", createdAdmin)
+
+    res.status(201)
+    res.send({
+      status: "SUCCESS",
+      message: "Account successfully created",
+      data: { jwtToken, admin: { id: createdAdmin._id } },
+    })
+  } catch (error) {
+    // console.log("Error:", error)
+    res.status(400)
+    res.send({ status: "FAIL", message: error })
+  }
+})
+
+app.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const existingAdmin = await Admin.findOne({
+      email: email.toLowerCase(),
+    })
+    console.log(existingAdmin)
+
+    if (existingAdmin) {
+      const hashedPassword = existingAdmin.password
+      const isPasswordCorrect = await bcrypt.compare(password, hashedPassword)
+
+      if (isPasswordCorrect) {
+        const jwtToken = jwt.sign(
+          { existingAdmin },
+          process.env.JWT_SECRET_KEY,
+          {
+            expiresIn: "15d",
+          }
+        )
+
+        res.status(200)
+        res.send({
+          status: "SUCCESS",
+          message: "Login Successful",
+          data: { jwtToken, Admin: { id: existingAdmin._id } },
+        })
+      } else {
+        throw "Incorrect password"
+      }
+    } else {
+      throw "Admin not found"
+    }
+  } catch (error) {
+    res.status(400)
+    res.send({
+      status: "FAIL",
+      message: error,
+    })
+  }
+})
+
 app.listen(process.env.PORT, async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL)
