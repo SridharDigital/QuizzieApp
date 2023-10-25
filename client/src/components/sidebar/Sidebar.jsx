db6f9321a25fa14f68512ace78827621f70d4f01
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2023-10-25 12:33:15 +0530

    Created Sidebar UI and imported it into Dashboard Page.

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,27 @@
+import style from "./Sidebar.module.css"
+
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  const isActive = true
+  return (
+    <div className={style.sidebarContainer}>
+      <h1 className={style.logo}>Quizzie</h1>
+      <ul className={style.menuItemContainer}>
+        <li
+          className={`${style.menuItem} ${isActive ? style.navActive : null}`}
+        >
+          Dashboard
+        </li>
+        <li className={style.menuItem}>Analytics</li>
+        <li className={style.menuItem}>Create Quiz</li>
+      </ul>
+      <div className={style.logoutSectionContainer}>
+        <div className={style.hrLineDiv}>
+          <hr />
+        </div>
+        <button className={style.sidebarLogoutBtn}>Logout</button>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,13 @@
+import Sidebar from "../../components/sidebar/Sidebar"
+import style from "./Dashboard.module.css"
+
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <div>
+      <Sidebar />
+      <div>Dashboard</div>
+    </div>
+  )
 }
 
 export default Dashboard
